c3609ac2d66fe36d5a280265494b74752286c5ab
Vitaly <Vitaly@DESKTOP-0IE18EK>
2024-05-22 19:36:44 +0300

    Добавьте файлы проекта.

---
+++ hw_new2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_new2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("В какой сфере деятельности работаете: ");
+            string profeccionalArea = Console.ReadLine();
+            Console.Write("Сколько времени уделяете времени программированию: ");
+            int studyTime = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Желаемая зароботная плата: ");
+            long futureSalary = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($"Ваше имя {name}, вы заняты в/на - {profeccionalArea}. Учитесь {studyTime} часов и хотите запрлату {futureSalary} тугриков.");
+        }
+    }
+}
